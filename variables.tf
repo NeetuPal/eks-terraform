d94808dd4c5b09316b59cee53c4f550dd53d0e2c
Neetu Pal <neetupalcs13@gmail.com>
2025-10-24 01:01:41 +0530

    add files

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "ssh_key_name" {
+  description = "The name of the SSH key pair to use for instances"
+  type        = string
+  default     = "keypair"
+}
+variable "developer_profile" {
+  default = "dev"
+}
